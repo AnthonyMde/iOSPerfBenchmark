a8156d9ebfede3352cf08aad7b96f1a953a06d3a
AnthonyMde <anthony.mamode@gmail.com>
2025-06-23 12:27:12 +0200

    move image list into component and own file

--- iOSPerfBenchmark/ContentView.swift
+++ iOSPerfBenchmark/ContentView.swift
@@ -7,71 +7,11 @@
 
 import SwiftUI
 
-// MARK: - Rotating Image Component
-struct RotatingImage: View {
-    let imageName: String
-    @State private var rotation: Double = 0
-    let timer = Timer.publish(every: 1/120, on: .main, in: .common).autoconnect()
-    
-    var body: some View {
-        Image(imageName)
-            .resizable()
-            .scaledToFit()
-            .rotationEffect(.degrees(rotation))
-            .onReceive(timer) { _ in
-                rotation += 3 // 360 degrees / 120 frames = 3 degrees per frame
-                if rotation >= 360 {
-                    rotation = 0
-                }
-            }
-    }
-}
-
-// MARK: - Horizontal Image List Component
-struct HorizontalImageList: View {
-    let images: [String]
-    
-    var body: some View {
-        ScrollView(.horizontal, showsIndicators: true) {
-            HStack(spacing: 0) {
-                ForEach(images, id: \.self) { imageName in
-                    RotatingImage(imageName: imageName)
-                        .frame(width: 50, height: 50)
-                }
-            }
-            .padding(.horizontal)
-        }
-        .background(Color(.systemGray6).opacity(0.3))
-    }
-}
-
-// MARK: - List Item Component
-struct ListItem: View {
-    let index: Int
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            HorizontalImageList(images: Array(repeating: ["image1", "image2", "image3"], count: 10).flatMap { $0 })
-        }
-    }
-}
-
-// MARK: - Main Content View
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            ScrollView {
-                LazyVStack(spacing: 0) {
-                    ForEach(0..<300, id: \.self) { index in
-                        ListItem(index: index)
-                    }
-                }
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .transaction { transaction in
-            transaction.animation = nil
-        }
+        ImageListBenchmark()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .transaction { $0.animation = nil }
     }
 }
 
---
+++ iOSPerfBenchmark/ImageListBenchmark.swift
@@ -0,0 +1,58 @@
+//
+//  ImageListBenchmark.swift
+//  iOSPerfBenchmark
+//
+//  Created by Anthony Mamode on 23/06/2025.
+//
+import SwiftUI
+
+// MARK: - MainListView
+struct ImageListBenchmark: View {
+    var body: some View {
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(0..<300, id: \.self) { index in
+                        HorizontalImageList(images: Array(repeating: ["image1", "image2", "image3"], count: 10).flatMap { $0 })
+                    }
+                }
+            }
+    }
+}
+
+// MARK: - Horizontal Image List Component
+struct HorizontalImageList: View {
+    let images: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(images, id: \.self) { imageName in
+                    RotatingImage(imageName: imageName)
+                        .frame(width: 50, height: 50)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .background(Color(.systemGray6).opacity(0.3))
+    }
+}
+
+// MARK: - Rotating Image Component
+struct RotatingImage: View {
+    let imageName: String
+    @State private var rotation: Double = 0
+    let timer = Timer.publish(every: 1/120, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFit()
+            .rotationEffect(.degrees(rotation))
+            .onReceive(timer) { _ in
+                rotation += 3 // 360 degrees / 120 frames = 3 degrees per frame
+                if rotation >= 360 {
+                    rotation = 0
+                }
+            }
+    }
+}
